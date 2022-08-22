b9adb743b1d9f1f16db161b5489ab9eccec6b707
jatin <sandilya.jatin@gmail.com>
2022-08-22 13:44:12 +0530

    Fix 5

--- lib/slack.ts
+++ lib/slack.ts
@@ -260,14 +260,14 @@ export const configureBlocks = (
       text: ":bulb: SUBREDDIT :bulb:",
     },
   },
-  // {
-  //   type: "section",
-  //   block_id: `keyword_${subReddit}`,
-  //   text: {
-  //     type: "mrkdwn",
-  //     text: "`" + subReddit + "`",
-  //   },
-  // },
+  {
+    type: "section",
+    block_id: `keyword_${subReddit}`,
+    text: {
+      type: "mrkdwn",
+      text: "`" + subReddit + "`",
+    },
+  },
   // {
   //   type: "input",
   //   dispatch_action: true,
